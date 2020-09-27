73a4da57ce526a77840794480928e2b4447522c5
tejaswishah1 <tejaswishah1998@gmail.com>
2020-09-27 19:37:46 +0530

    [tejas] add welcome file added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Geometry_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Geometry assignment");
+        }
+    }
+}
