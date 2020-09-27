4bccf6a7ad04f42163d00b0b428cf1c5e482b6b9
tejaswishah1 <tejaswishah1998@gmail.com>
2020-09-27 19:47:36 +0530

    [tejas] add message calculated length of line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,43 @@ namespace Geometry_assignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Geometry assignment");
+
+            ////Declaring the Variables:
+            int[] line1Pt1 = new int[2];
+            int[] line1Pt2 = new int[2];
+            int[] line2Pt1 = new int[2];
+            int[] line2Pt2 = new int[2];
+
+
+
+            ////Function to find length
+            static double FindLength(int[] l1, int[] l2)
+            {
+                double length;
+                length = Math.Sqrt(Math.Pow((l2[0] - l1[0]), 2) + Math.Pow((l2[1] - l1[1]), 2));
+                return length;
+            }
+
+            ////Provide Inputs for co-ordinates of first line:         
+            static void InputLine(ref int[] point1, ref int[] point2)
+            {
+                Console.WriteLine("Enter X-Coordinate of 1st point");
+                point1[0] = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Y-Coordinate of 1st point");
+                point1[1] = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter X-Coordinate of 2nd point");
+                point2[0] = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Y-Coordinate of 2nd point");
+                point2[1] = Int32.Parse(Console.ReadLine());
+
+                return;
+            }
+
+            ////Taking co-ordinates of line1 and printing length of line 1
+            InputLine(ref line1Pt1, ref line1Pt2);
+            double line1Length;
+            line1Length = FindLength(line1Pt1, line1Pt2);
+            Console.WriteLine("Length of first line is " + line1Length);
         }
     }
 }
